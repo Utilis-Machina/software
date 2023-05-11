42393f46eb75fcecde0f0e060c103ad92b5bef69
ssulack <130875503+ssulack@users.noreply.github.com>
2023-05-11 09:38:42 -0700

    Update power_check.py

--- PowerCheck/power_check.py
+++ PowerCheck/power_check.py
@@ -1,18 +1,34 @@
 r"""Serial interface wrapper to PWRCheck+ device using the binary interface.
 
-This python interface provides configuration and data collection functions
-for the West Mountain Radio Powercheck+ device. The device can be powered via
-micro-USB or when connected to a power supply.
+This library is a platform independent serial interface to the PowerCheck+
+power monitor device by West Mountain Radio.
+Copyright (C) 2023  Stephen Sulack
 
-The power check device has a serial interface that is used in this wrapper.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Communication is done via micro USB, which also powers the device. If
+a voltage source is available it will run off that instead.
+
 In order to make it easier to manage on linux, it is recommended to
 create a udev rule in /etc/udev/rules.d (suggest 88-utilis.rules), but
 without any linebreaks:
 ACTION=="add", ATTRS{idVendor}=="2405", ATTRS{idProduct}=="0001",
- ATTRS{product}=="PWRCheck+", MODE="0660", SYMLINK+="PwrCheck"
+ATTRS{product}=="PWRCheck+", MODE="0660", SYMLINK+="PwrCheck"
 
 This will make sure whenever one is plugged in /dev/PwrCheck will be an
-available device. The code was tested on windows and linux.
+available device. The code was tested on windows and linux, using the
+FW version 1.27.
 """
 import collections
 import os
@@ -39,8 +55,17 @@ class PowerCheck:
     """
     # The byte mapping and fields for unit info message.
     UNIT_INFO_FMT = '<BBB'
+    # Note that the versions are tracked in hex on the display, so
+    # version 1.27 has a integer value of 39 for minor.
     UnitInfo = collections.namedtuple(
         'UnitInfo', ['major', 'minor', 'revision'])
+
+    # The byte information for the repeated dump message.
+    DUMP_FMT = '<Ii'
+    DumpInfo = collections.namedtuple('DumpInfo', ['voltage_mv', 'current_ma'])
+
+    # General byte packet structure.
+    PacketInfo = collections.namedtuple('PacketInfo', ['type', 'payload'])
 
     # The byte mapping and fields for the config message.
     CONFIG_FMT = '<BBIIIIBiiiiBH'
@@ -78,17 +103,19 @@ class PowerCheck:
         'reverse_ma_hrs']  # Load to source flow.
     )
 
-    # Selects which values from the status message to log to file when streaming.
+    # Selects values from the status message to log to file when streaming.
     _LOG_VALUES = ('calibrated_voltage_mv', 'calibrated_current_ma', 'ma_hours',
                    'quality_mv', 'forward_ma_hrs', 'reverse_ma_hrs')
 
-    def __init__(self, port: str, baudrate: int = 115200, monitor_file_location: Optional[str] = None):
+    def __init__(self, port: str = 'COM3', baudrate: int = 115200,
+                 monitor_file_location: Optional[str] = None):
         """Sets up a power check device using interface information provided.
 
         Args:
           port: the path to device on machine.
           baudrate: the rate to communicate with the device.
-          monitor_file_location: if provided, will open a file to receive streamed data.
+          monitor_file_location: if provided, will open a file to receive
+            streamed data.
         """
         # Open device.
         self._ser = serial.Serial(
@@ -97,12 +124,17 @@ class PowerCheck:
         self._monitor_file = None
         if monitor_file_location:
             self.set_status_file(monitor_file_location)
-        self._num_log_values = len(self._LOG_VALUES)  # Number of entries to put in file.
+        # Number of entries to put in file.
+        self._num_log_values = len(self._LOG_VALUES)
 
         # The next few items cache values requested from the device.
         self._last_config = None
         self._last_status = None
         self._unit_info = None
+
+        # These are for debugging the raw traffic.
+        self._read_buffer = collections.deque(maxlen=10)
+        self._write_buffer = collections.deque(maxlen=10)
 
         self._ser.flush()
 
@@ -117,14 +149,87 @@ class PowerCheck:
         if self._last_status is None:
             self.get_status()
         return self._last_status
-    
+
     @property
     def unit_info(self) -> UnitInfo:
         if self._unit_info is None:
             self.get_unit_info()
         return self._unit_info
 
-    def _make_packet(self, msg_type: int, payload: Optional[bytes] = None) -> bytes:
+    def _write(self, value: bytes):
+        """Writes and stores raw serial values."""
+        self._ser.write(value)
+        self._write_buffer.append(value)
+
+    def dump_read_buffer(self) -> collections.deque:
+        return self._read_buffer
+
+    def dump_write_buffer(self) -> collections.deque:
+        return self._write_buffer
+
+    def dump_eeprom(self) -> list[tuple[int, int]]:
+        """Initiates and manages dump of eeprom on device.
+
+        When the unit EEPROM is dumped, payloads will be assembled to send the
+        data in chunks. From testing this seems to be 12 entries at a time, so
+        with 8 bytes per entry, the checksum, and packet type, they come in up
+        to 98 byte payload chunks. The last packet indicates EOF by sending an
+        empty payload.
+
+        The unit can store more than 750K points, so this can take a while.
+
+        Returns:
+          A list of tuples, the voltage and current values from dump.
+        """
+        request_dump = self._make_packet(0xa2)
+        # We need to send an accept and complete nack after each block of data.
+        nack_accept = self._nack_packet()
+        nack_complete = self._nack_packet(1)
+        data = []
+        cur_packet = self.PacketInfo(0x0, b'\x01')
+        # Initiate the transfer.
+        self._write(request_dump)
+        # Read one packet at a time until we reach EOF.
+        while cur_packet.payload:
+            time.sleep(0.01)
+            cur_packet = self._read_one_packet(type=0xa3)
+            data.append(cur_packet.payload)
+            self._write(nack_accept)
+            self._write(nack_complete)
+        return self._process_dump_raw_data(data)
+
+    def _read_one_packet(self, type: Optional[int] = None) -> PacketInfo:
+        """Reads one packet from the device."""
+        # Read to start of packet.
+        while 1:
+            self._ser.read_until(b'\x80')
+            packet_len = self._ser.read(1)
+            packet_data = self._ser.read(int.from_bytes(packet_len, 'little'))
+            packet_checksum = packet_data[-1]
+            # Validate all data received.
+            if packet_checksum != sum(packet_len + packet_data[:-1]) & 0xff:
+                print('Checksum mismatch:')
+                print(packet_len + packet_data)
+            else:
+                self._read_buffer.append(packet_len + packet_data)
+            # Separate the type and remove the checksum from the end.
+            packet_result = self.PacketInfo(packet_data[0], packet_data[1:-1])
+            if packet_result.type == type or type is None:
+                break
+
+        return packet_result
+
+    def _process_dump_raw_data(self,
+                               dump_bytes: bytes) -> list[tuple[int, int]]:
+        """Processes the raw bytes received during a data dump."""
+        values = []
+        # Skip last value, which is null.
+        for p in dump_bytes[:-1]:
+            values += list(struct.iter_unpack(self.DUMP_FMT, p))
+        return values
+
+    def _make_packet(self, msg_type: int,
+                     payload: Optional[bytes] = None) -> bytes:
         """Makes a binary packet to send to device."""
         hdr = 0x80
         # Length, checksum are required for all packets.
@@ -137,9 +242,9 @@ class PowerCheck:
         packet.append(sum(packet[1:]) & 0xff)
         return bytes(packet)
 
-    def _process_packet(self, fmt: str, start_byte: int, packet: bytes):
+    def _process_packet(self, fmt: str, packet: PacketInfo):
         """Processes packet and returns values.
-        
+
         Convenience method to index into packet, extract values, and return
         the converted result.
 
@@ -148,14 +253,14 @@ class PowerCheck:
           start_byte: the type of message, which proceeds the payload to process.
           packet: the bytes to process from the unit.
         Returns:
-          The extracted values from bytes after applying fmt.
+        The extracted values from bytes after applying fmt.
 
         """
-        return struct.unpack_from(fmt, packet, offset=packet.index(start_byte) + 1)
+        return struct.unpack_from(fmt, packet.payload)
 
     def _nack_packet(self, code: int = 0x0) -> bytes:
         """Gets a nack packet to send to device.
-        
+
         Args:
           code: the status to relay.
             0 - packet accepted, link layer acceptance of packet.
@@ -168,16 +273,18 @@ class PowerCheck:
 
     def _send_nack(self, code: int = 0x0):
         """Sends an acknowledgement of packet."""
-        self._ser.write(self._nack_packet(code))
+        self._write(self._nack_packet(code))
 
-    def _packet_handshake(self, packet_type: int, data_fmt: str, 
-                          data_type: collections.namedtuple) -> collections.namedtuple:
+    def _packet_handshake(
+            self, packet_type: int, data_fmt: str,
+            data_type: collections.namedtuple) -> collections.namedtuple:
         """Requests data from device and returns processed data type class.
 
         For the get methods, there is a basic pattern to repeat:
         1. Send packet requesting packet type N.
         2. Receive response of type N+1 from device.
-        3. Respond with ACK for packet within 250ms to prevent resends of packet.
+        3. Respond with ACK for packet within 250ms to prevent resends of
+          packet.
 
         Args:
           packet_type: the numeric code of the packet being requested.
@@ -187,48 +294,67 @@ class PowerCheck:
           An instance of the data_type provided.
         """
         request = self._make_packet(packet_type)
-        self._ser.write(request)
+        self._write(request)
         time.sleep(NACK_SLEEP)
-        response = self._ser.read_all()
-        print(f'Response: {response}')
-        raw_packet = self._process_packet(data_fmt, packet_type + 1, response)
+        response = self._read_one_packet(packet_type + 1)
+        raw_packet = self._process_packet(data_fmt, response)
         self._send_nack()
         return data_type(*raw_packet)
 
+    def get_unit_info(self):
+        """Gets unit firmware information (0xf1)."""
+        self._unit_info = self._packet_handshake(
+            0xf1, self.UNIT_INFO_FMT, self.UnitInfo)
+        return self._unit_info
+
     def get_config(self) -> Config:
         """Gets config (0xa4) for device and returns values."""
-        self._last_config = self._packet_handshake(0xa4, self.CONFIG_FMT, self.Config)
+        self._last_config = self._packet_handshake(
+            0xa4, self.CONFIG_FMT, self.Config)
         return self._last_config
 
     def set_config(self, **kwargs):
-        """Sets configuration items provided from config namedtuple."""
+        """Sets configuration items provided from config namedtuple.
+
+        This function can be used to set config using the fields of the
+        config namedtuple, in any number. For example:
+          unit.set_config(backlight_timeout_s=10, log_interval_s=1)
+
+        First it fetches the current config, modifies the fields requested,
+        and then sends them back to the device.
+
+        Disclaimer: currently this sets the foreground and background colors
+        of the screen to black. This is most likely due to the reserved bytes
+        picking up new functionality that is not reflected in the design doc.
+        """
         # Build the message based off last known values.
         packet_type = 0xa6
         config_dict = self.config._asdict()
         for key, val in kwargs.items():
             config_dict[key] = val
-        # Convert back to namedtuple.
+        # Convert back to namedtuple to order fields properly.
         new_config = self.Config(**config_dict)
-        print(f'Now config = {new_config}')
-        # Create payload.
         payload = struct.pack(self.CONFIG_FMT, *new_config)
-        # Add 9 trailing unpopulated bytes.
+        # Add 9 trailing unpopulated bytes per design doc.
         payload += 9 * b'\x00'
         request = self._make_packet(packet_type, payload)
-        print(f'Send: {request}')
-        self._ser.write(request)
+        self._write(request)
         time.sleep(NACK_SLEEP)
-        response = self._ser.read_all()  # Should get a nack + complete message.
-        print(f'Received: {response}')
+        accept = self._read_one_packet()  # Read acceptance.
+        complete = self._read_one_packet()  # Read complete.
+        if accept == 0 and complete == 1:
+            # If the packet was accepted and completed update cached config.
+            self._last_config = new_config
 
     def get_status(self):
         """Gets status (0xa0) measured on device."""
-        self._last_status = self._packet_handshake(0xa0, self.STATUS_FMT, self.Status)
+        self._last_status = self._packet_handshake(
+            0xa0, self.STATUS_FMT, self.Status)
         return self._last_status
-    
+
     def set_status_file(self, status_path: str):
         """Creates a file to receive streamed unit data.
-        
+
         This can be defined after the object is constructed. If this
         is run and stream is called, the values collected from the
         unit will be copied to this file.
@@ -248,24 +374,22 @@ class PowerCheck:
         self._monitor_file.write(
             'time,' + ','.join(self._LOG_VALUES) + '\n')
 
-    def stream_status(self, sample_period_s: float = 1.0, num_samples: int = 10):
+    def stream_status(self, sample_period_s: float = 1.0,
+                      num_samples: int = 10, echo: bool = False):
         """Streams status from device."""
+        write_fmt = '{:f}' + ',{:d}'*self._num_log_values + '\n'
         for i in range(num_samples):
             status = self.get_status()
-            print(status)
+            if echo:
+                print(status)
             time.sleep(sample_period_s)
             if self._monitor_file:
-                new_entry = [time.time()] + [getattr(status, e) for e in self._LOG_VALUES]
-                self._monitor_file.write(
-                    ('{:f}' + ',{:d}'*self._num_log_values + '\n').format(*new_entry))
+                new_entry = [time.time()] + [getattr(status, e)
+                                             for e in self._LOG_VALUES]
+                self._monitor_file.write((write_fmt).format(*new_entry))
 
-    def get_unit_info(self):
-        """Gets unit firmware information (0xf1)."""
-        self._unit_info = self._packet_handshake(0xf1, self.UNIT_INFO_FMT, self.UnitInfo)
-        return self._unit_info
-
-    def reset_all_counters(self):
-        """Resets system counters for specified element."""
+    def clear_log_data(self):
+        """Clears log data stored on EEPROM of device."""
         packet_type = 0xa8
         # Bits are:
         #  b0: Amp hours.
@@ -275,10 +399,11 @@ class PowerCheck:
         #  b4: Max Current.
         #  b5: Quality.
         #  b6: Clear all log data.
-        payload = struct.pack('<B', 0b100000)
+        payload = struct.pack('<B', 0b1000000)
         request = self._make_packet(packet_type, payload)
-        print(f'Send: {request}')
-        self._ser.write(request)
+        self._write(request)
         time.sleep(NACK_SLEEP)
-        response = self._ser.read_all()  # Should get a nack.
-        print(f'Received: {response}')
+        accept = self._read_one_packet()  # Acceptance of command.
+        complete = self._read_one_packet()  # Completion of command.
+        if accept != 0 and complete != 1:
+            raise ValueError('Reset command not accepted.')
